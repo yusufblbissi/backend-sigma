ba69bc884152b1333718a584e0f48f869180d3cc
yousef balbisi <95571473+yusufblbissi@users.noreply.github.com>
2024-11-14 12:19:42 +0200

    Update loanRoutes.js

--- routes/loanRoutes.js
+++ routes/loanRoutes.js
@@ -10,6 +10,29 @@ import { validationResult } from "express-validator";
 
 const router = express.Router();
 
+/**
+ * @swagger
+ * /submit:
+ *   post:
+ *     summary: Submit a loan application
+ *     tags: [Loan Applications]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               amount:
+ *                 type: number
+ *               currency:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Loan application submitted successfully
+ *       400:
+ *         description: Invalid input
+ */
 router.post("/submit", loanValidation, (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
@@ -18,10 +41,56 @@ router.post("/submit", loanValidation, (req, res) => {
   submitLoanApplication(req, res);
 });
 
+/**
+ * @swagger
+ * /listOfCurrencies:
+ *   get:
+ *     summary: List available loan currencies
+ *     tags: [Loan Applications]
+ *     responses:
+ *       200:
+ *         description: List of available currencies
+ */
 router.get("/listOfCurrencies", listOfCurrencies);
 
+/**
+ * @swagger
+ * /{id}:
+ *   get:
+ *     summary: Get loan applications by ID
+ *     tags: [Loan Applications]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Loan application details
+ *       404:
+ *         description: Loan application not found
+ */
 router.get("/:id?", getLoanApplications);
 
+/**
+ * @swagger
+ * /{id}:
+ *   delete:
+ *     summary: Delete a loan application by ID
+ *     tags: [Loan Applications]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Loan application deleted successfully
+ *       500:
+ *         description: Error deleting loan application
+ */
 router.delete("/:id", (req, res) => {
   const { id } = req.params;
   deleteLoanApplication(id)
@@ -30,13 +99,5 @@ router.delete("/:id", (req, res) => {
       res.status(500).send("Error deleting loan application: " + error.message)
     );
 });
-
-// router.put("/:id", loanValidation, (req, res) => {
-//   const errors = validationResult(req);
-//   if (!errors.isEmpty()) {
-//     return res.status(400).json({ errors: errors.array() });
-//   }
-//   updateLoanApplication(req, res);
-// });
 
 export default router;
